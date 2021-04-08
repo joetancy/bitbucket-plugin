878d6cb0f1077cf08a70ea63a206f8e54b1bb4aa
Joe Tan <joetanchinyong@gmail.com>
2021-04-08 14:32:57 +0800

    prelim secret webhooks

--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketHookReceiver.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketHookReceiver.java
@@ -8,9 +8,16 @@ import java.net.URLDecoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 import net.sf.json.JSONObject;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
@@ -43,8 +50,31 @@ public class BitbucketHookReceiver implements UnprotectedRootAction {
         String body = IOUtils.toString(req.getInputStream());
         if (!body.isEmpty() && req.getRequestURI().contains("/" + BITBUCKET_HOOK_URL + "/")) {
             String contentType = req.getContentType();
-            if (contentType != null && contentType.startsWith("application/x-www-form-urlencoded")) {
-                body = URLDecoder.decode(body);
+            if (contentType != null && contentType.startsWith("application/json")) {
+                String key = "SECERT_KEY";
+                String signature = req.getHeader("X-Hub-Signature");
+                if (signature != null) {
+                    signature = signature.substring(7);
+                    try {
+                        HMac hmac = new HMac(new SHA256Digest());
+                        hmac.init(new KeyParameter(key.getBytes()));
+                        byte[] result = new byte[hmac.getMacSize()];
+                        byte[] bytes = body.getBytes();
+                        hmac.update(bytes, 0, bytes.length);
+                        hmac.doFinal(result, 0);
+                        String hash = Hex.encodeHexString(result);
+                        LOGGER.log(Level.INFO, "SHA256HMAC Computed: {0}", hash);
+                        if (hash.equals(signature)) {
+                            LOGGER.log(Level.INFO, "SHA256HMAC Computed verifies the X-Hub-Signature header");
+                        } else {
+                            LOGGER.log(Level.INFO, "SHA256HMAC Computed does not match the X-Hub-Signature header! Skip processing of webhook!");
+                            return;
+                        }
+                    }
+                    catch (Exception e){
+                        LOGGER.log(Level.INFO, "Unable to compute SHA256HMAC for signature validation.");
+                    }
+                }
             }
             if (body.startsWith("payload=")) body = body.substring(8);
 
